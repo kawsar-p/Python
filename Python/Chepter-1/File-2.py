8e8e869147ec6cec3a307b0c9289d0c305458d77
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-01-27 19:24:27 +0600

    File-2

---
+++ Python/Chepter-1/File-2.py
@@ -0,0 +1,16 @@
+# Variable
+
+A = 10
+B = 20.5
+C = "Charecter"
+
+"""
+print(A)
+print(B)
+prinrt(C)
+
+"""
+print ("Integer type value = ", A)
+print ("Float type value = ", B)
+print ("Charecter type value = ", C)
+print("Integer, float and Charecter type value =", A, B, C)
