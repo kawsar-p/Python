e6e7f3838c954bb196bf62566ba9ddca652be36a
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-01-27 19:04:18 +0600

    First python program

--- Python/Chepter-1/File-1.py
+++ Python/Chepter-1/File-1.py
@@ -1,1 +1,14 @@
+# First python program ( Single line Comments)
 
+print('Hello world')
+print("Hello world")
+
+
+# Multiple line comments
+
+"""
+print("Hello, My Name is Md. Kawsar Miah")
+print("I am a student of computer science and enginnering")
+print(" IUBAT- international universiry of business agriculture and technology")
+"""
+# Output: Hello World!
