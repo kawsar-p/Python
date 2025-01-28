3decf43c712156cad32c90670326277fb615e7c3
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-01-28 16:47:34 +0600

    New File

---
+++ Python/Chepter-1/File-6.py
@@ -0,0 +1,12 @@
+# Math related library function
+
+from math import *    # Import all math library function
+
+print("Maximum value: ",max(10,20,30,-20))
+print("Mininmum value: ",min(10,20,30,-20))
+print("ABS value: ",abs(-4))
+print("Power value: ",pow(2,3))
+print("Square Root value: ",sqrt(4))
+print("Round value: ",round(10.65))
+print("Float value: ",float(10.65))
+print("Ceil value: ",ceil(5.65))
