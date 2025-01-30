5b16c614595a5fdbb513b60f9d7be116f1a30ac9
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-01-30 23:12:47 +0600

    New loops

---
+++ Python/Loop/While loop/File-3.py
@@ -0,0 +1,36 @@
+# Addition of unlimitade value:
+
+n = int(input("Enter your value: "))
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i + 1
+    # print("Results: ", sum) # Full loops show
+
+print("Results: ", sum) # Final loops results
+
+
+"""
+while i <= n:
+    sum = sum + i
+    i = i + 2 # Even Addition
+
+
+print("Results: ", sum) # Final loops results
+
+"""
+
+# All same as Even loops 
+# i = 0 # only start form 0
+
+"""
+while i <= n:
+    sum = sum + i
+    i = i + 2 # Even Addition
+
+
+print("Results: ", sum) # Final loops results
+
+"""
