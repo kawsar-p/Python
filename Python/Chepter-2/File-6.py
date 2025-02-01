dcda8f7abb3718191eebc132850400443d407604
mdkawsarmiah <mdkawsarmiah33@gmail.com>
2025-02-01 14:52:52 +0600

    Range

---
+++ Python/Chepter-2/File-6.py
@@ -0,0 +1,19 @@
+# Range function
+
+iteam = list(range(10))
+print("Index number = ", iteam)
+print(iteam[2]) # Range by index number
+
+# Range by starting point and ending point
+
+iteam1 = list(range(2,20)) # Start = 2 ; End = 20
+print(iteam1)
+
+# Range by start end and gap between two value
+
+
+iteam2 = list(range(2,20,2)) # Start = 2 ; End = 20 ; Gap
+print(iteam2)
+
+
+
