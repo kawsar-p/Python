e8004f746879cc993f4de9652ecf4e4dcad9d30d
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-08-07 02:51:16 +0600

    update

--- Data camp Python/Basic/Calculator.py
+++ Data camp Python/Basic/Calculator.py
@@ -1,2 +1,11 @@
-a = 1+3
-print(a)
+# Addition
+print("Addition: ", 5 + 3)
+
+# Subtraction
+print("Subtraction: ", 10 - 4)
+
+# Multiplication
+print("Multiplication: ", 6 * 7)
+
+# Division
+print("Division: ", 20 / 5)
---
+++ Data camp Python/Basic/Data type.py
@@ -0,0 +1,11 @@
+A = 10
+B = 5.9
+C = None
+D = 'Hello'
+E = True or False
+
+print(type(A))  # Should output: <class 'int'>
+print(type(B))  # Should output: <class 'float'>
+print(type(C))  # Should output: <class 'NoneType'>
+print(type(D))  # Should output: <class 'str'>
+print(type(E))  # Should output: <class 'bool'>
