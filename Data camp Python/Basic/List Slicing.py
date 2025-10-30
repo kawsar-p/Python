f64fe6f14f254fec617e9403935752d75fa33311
Md. Kawsar Miah <mdkawsarmiah33@gmail.com>
2025-10-30 13:06:42 +0600

    Delete Data camp Python/Basic directory

--- Data camp Python/Basic/List Slicing.py
+++
@@ -1,9 +0,0 @@
-list = ["apple", "banana", "cherry"]
-print(list)
-# Modifying the list
-print(list[1])
-print(list[0:2])  # Slicing the list
-print(list[-1])  # Accessing the last element
-print(list[2:])
-print(list[:2])  # Accessing the first two elements
-print(list[1:3])  # Accessing the second and third elements
